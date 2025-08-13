5cb14134f218b69f7e121508afe9ddba36b8fb65
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2025-08-13 20:43:09 +0530

    added the function to change the dice image every time the site is refreshed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var n1= Math.floor(Math.random()*6)+1;
-var n2= Math.floor(Math.random()*6)+1;
+var n1=Math.floor(Math.random()*6)+1;
+var n2=Math.floor(Math.random()*6)+1;
+
+function Dice_change(n1 , n2){
+    document.querySelector(".img1 img").setAttribute("src" , "./images/dice" +n1+".png")
+
+    document.querySelector(".img2 img").setAttribute("src" , "./images/dice" +n2+".png")
+    
+}
+Dice_change(n1 ,n2);
